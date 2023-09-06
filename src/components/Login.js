b72f796ff4a9798029b1f0eb83fac10cf9114ced
David <86584824+David6811@users.noreply.github.com>
2023-09-06 21:00:21 +1000

    Remove login css (#2)
    * Using Bootstrap style instead of Login.css
    
    * Using Bootstrap style instead of Login.css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,57 @@
 import React from 'react';
 import { Button, Stack } from 'react-bootstrap';
-import './Login.css';
+//import './Login.css';
 
 function Login(props) {
   return (
-    <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Welcome to our React Tutorial</h3>
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter password"
-            />
-          </div>
-          <div className="form-group mt-2">
-            <p>or <a href="#">Sign up?</a></p>
-          </div>
 
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Log In
-            </button>
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <div>
+                <form>
+                  <div>
+                    <h3>Welcome to our React Tutorial</h3>
+                    <div className="form-group mt-3">
+                      <label>Email address</label>
+                      <input
+                        type="email"
+                        className="form-control mt-1"
+                        placeholder="Enter email"
+                      />
+                    </div>
+                    <div className="form-group mt-3">
+                      <label>Password</label>
+                      <input
+                        type="password"
+                        className="form-control mt-1"
+                        placeholder="Enter password"
+                      />
+                    </div>
+                    <div className="form-group mt-2">
+                      <p>or <a href="#">Sign up?</a></p>
+                    </div>
+
+                    <div className="d-grid gap-2 mt-3">
+                      <button type="submit" className="btn btn-primary">
+                        Log In
+                      </button>
+                    </div>
+                    <p className="forgot-password text-right">
+                      Forgot <a href="#">password?</a>
+                    </p>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
-          <p className="forgot-password text-right">
-            Forgot <a href="#">password?</a>
-          </p>
         </div>
-      </form>
+      </div>
     </div>
+
+
   )
 }
 
