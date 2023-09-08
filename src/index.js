ce431b063bb23a34828ef644a2cef9f3e480ba25
David <86584824+David6811@users.noreply.github.com>
2023-09-08 20:46:20 +1000

    Integrate parse server for sign up (#3)
    * enable React Routes
    
    * add signup.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './components/Signup';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path='/signup' element={<Signup />} />
+    <Route path='/login' element={<Login />} />
+  </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
